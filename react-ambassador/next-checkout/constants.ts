4979b99f7b7c89d7056ba2cb5346b8ee3694e79d
kravtsov <shrituon@gmail.com>
2023-09-28 14:56:37 +0300

    initial commit

---
+++ react-ambassador/next-checkout/constants.ts
@@ -0,0 +1,4 @@
+export default {
+    endpoint: 'http://localhost:8000/api/checkout',
+    stripe_key: 'pk_test_51NvEYWI46FFwFAHYGBTUrXBGqM2EsBoXSo89zjdWRtjcQdSCPYD2bGyIRKraZTUnrQ07dpg6981Ys47sFAGmIZTC00kw3iUY7Y'
+}
